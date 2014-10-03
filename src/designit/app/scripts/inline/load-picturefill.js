53dd387ca97803c1b05a7439535446785ce1ad15
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2014-10-03 16:57:13 +0200

    Only picturefill if not supported + trigger download of first slide

--- src/designit/app/scripts/inline/load-picturefill.js
+++ src/designit/app/scripts/inline/load-picturefill.js
@@ -1,11 +1,11 @@
 // Load picturefill if not supported in browser
 (function(document) {
     'use strict';
-    if (!('srcset' in document.createElement('img'))) {
+    if (!(window.HTMLPictureElement)) {
         var element = document.createElement('script');
         var ref = document.getElementsByTagName('script')[0];
         element.async = 1;
-        element.src = '//cdnjs.cloudflare.com/ajax/libs/picturefill/2.1.0/picturefill.min.js';
+        element.src = 'bower_components/picturefill/dist/picturefill.min.js';
         ref.parentNode.insertBefore(element, ref);
     }
 }(document));
